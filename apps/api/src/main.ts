7da2e77423297dad0513ec9dee1f2f9baf8b03ae
ha.hao.minh <minhhahao@gmail.com>
2024-08-14 13:59:06 +0700

    feat: add http request logger

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api/docs', app, document);
   app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(3000);
--- libs/middlewares/src/request.logger.middleware.spec.ts
+++ libs/middlewares/src/request.logger.middleware.spec.ts
@@ -4,12 +4,17 @@ import { Request, Response, NextFunction } from 'express';
 
 interface CustomRequest extends Request {
   id: string;
+  ip: string;
+  method: string;
+  path: string;
 }
+
+interface CustomResponse extends Response {}
 
 describe('RequestIdMiddleware', () => {
   let middleware: RequestIdMiddleware;
   let req: CustomRequest;
-  let res: Response;
+  let res: Response & CustomResponse;
   let next: NextFunction;
 
   beforeEach(async () => {
@@ -19,13 +24,20 @@ describe('RequestIdMiddleware', () => {
 
     middleware = module.get<RequestIdMiddleware>(RequestIdMiddleware);
     req = {} as CustomRequest;
-    res = {} as Response;
+    res = {} as CustomResponse;
     next = jest.fn();
   });
 
   it('should set request id and response header', () => {
     const setMock = jest.fn();
-    res = { set: setMock } as unknown as Response;
+    res = {
+      set: setMock,
+      on: jest.fn((event, callback) => {
+        if (event === 'close') {
+          callback();
+        }
+      }),
+    } as unknown as Response;
     middleware.use(req, res, next);
     expect(req.id).toBeDefined();
     expect(setMock).toHaveBeenCalledWith({ 'x-request-id': req.id });
